3a233b4a4ab97eb73518e29f5882bbdb10296136
dfitriana28 <111998066+dfitriana28@users.noreply.github.com>
2023-11-02 11:44:40 +0700

    controller migration

---
+++ database/migrations/2023_11_02_042500_create_notulensis_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notulensis', function (Blueprint $table) {
+            $table->id();
+            $table->string('sidang_rapat');
+            $table->date('hari_tanggal');
+            $table->timestamps('jam_panggilan');
+            $table->string('jam_sidang_rapat');
+            $table->string('acara');
+            $table->string('ketua');
+            $table->string('sekretaris');
+            $table->string('pencatat');
+            $table->string('peserta_sidang');
+            $table->string('kata_pembuka');
+            $table->string('pembahasan');
+            $table->string('keputusan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notulensis');
+    }
+};
