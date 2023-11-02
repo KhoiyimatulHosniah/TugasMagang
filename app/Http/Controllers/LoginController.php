39bf32d2cd104facb2cdbbdb8cd48d653df55888
Khoiyimatul Hosniah <khoiyimatulhosniah23@gmail.com>
2023-11-02 13:32:17 +0700

    login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    public function showLoginForm()
+    
+    {
+        return view('login');
+    }
+
+
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/login');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*Login*/
+Route::get('/login', 'LoginController@showLoginForm')->name('login');
+Route::post('/login', 'LoginController@login');
+Route::post('/logout', 'LoginController@logout')->name('logout');
